a124dc11acc372fa8cd6ed204a97082ea67228c8
Fernando Rodriguez <frodriguezsidro@gmail.com>
2019-08-08 23:38:58 +0200

    Localhost URL database

--- src/main/java/com/dcnutrition/devapps/apirest/SpringBootDcnutritionApirestApplication.java
+++ src/main/java/com/dcnutrition/devapps/apirest/SpringBootDcnutritionApirestApplication.java
@@ -2,9 +2,11 @@ package com.dcnutrition.devapps.apirest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootDcnutritionApirestApplication {
+public class SpringBootDcnutritionApirestApplication
+extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDcnutritionApirestApplication.class, args);
